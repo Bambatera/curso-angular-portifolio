91172461a370dc23ea6ce6dad69a57fc7f7d68a3
Leandro Menezes <lmsilv@gmail.com>
2025-05-03 00:34:29 -0300

    Implementação do componente Projects
    - Adicionada a estrutura de SCSS para o componente.
    - Implementada a funcionalidade de abertura de diálogo ao clicar nos projetos.
    - Ajustes no HTML para incluir a interação com o botão.

--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/projects.interface';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { EDialogPanelClass } from '../../enum/e-dialog-panel-class';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
 	selector: 'app-projects',
 	standalone: true,
-	imports: [],
+	imports: [MatDialogModule],
 	templateUrl: './projects.component.html',
 	styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
 	public projects = signal<IProjects[]>([
 		{
 			image: '/assets/img/projects/vfull.png',
@@ -23,4 +28,11 @@ export class ProjectsComponent {
 			},
 		},
 	]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPanelClass.PROJECTS
+    });
+  }
 }
